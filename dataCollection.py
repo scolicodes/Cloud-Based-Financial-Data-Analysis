4ec380ca9ad3dd78c06b6bcfe3a30ea91a48a1f4
Michael Scoli <mscoli44@gmail.com>
2024-11-12 13:58:50 -0500

    Remove secret key from history

--- dataCollection.py
+++ dataCollection.py
@@ -1,8 +1,14 @@
 import requests
 import pandas as pd
 from azure.storage.blob import BlobServiceClient
+from dotenv import load_dotenv
+import os
 
-apiKey = '***REMOVED***'
+load_dotenv()  # Load environment variables from .env file
+
+
+apiKey = os.getenv("API_KEY")
+connectStr = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
 symbol = 'MNMD'
 url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={apiKey}'
 
@@ -20,7 +26,7 @@ df.to_csv('financial_data.csv', index=False)
 
 #uploaf CSV to Blob Storage
 connect_str = 'DefaultEndpointsProtocol=https;AccountName=bnyprojectstorage;AccountKey=***REMOVED***;EndpointSuffix=core.windows.net'
-blob_service_client = BlobServiceClient.from_connection_string(connect_str)
+blob_service_client = BlobServiceClient.from_connection_string(connectStr)
 container_client = blob_service_client.get_container_client("market-data")
 
 with open("financial_data.csv", "rb") as data:
