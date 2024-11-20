5d1ad310314d1e621930a208546792c5c9ceb9c6
Michael Scoli <mscoli44@gmail.com>
2024-11-20 13:15:20 -0500

    reverted runonstartup parameter to False

--- function_app.py
+++ function_app.py
@@ -8,7 +8,7 @@ import os
 
 app = func.FunctionApp()
 
-@app.timer_trigger(schedule="0 0 11 * * *", arg_name="myTimer", run_on_startup=True, use_monitor=False)
+@app.timer_trigger(schedule="0 0 11 * * *", arg_name="myTimer", run_on_startup=False, use_monitor=False)
 def timer_trigger(myTimer: func.TimerRequest) -> None:
     if myTimer.past_due:
         logging.info('The timer is past due!')
